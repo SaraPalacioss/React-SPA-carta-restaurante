ad94cfabf7708d0b2008de12cd639aa44e6bef33
sarastackhouse <sarapalacios7@gmail.com>
2021-03-18 10:54:40 +0100

    FormularioNuevo component basic structure done

--- src/components/Formularios/FormularioNuevo.js
+++ src/components/Formularios/FormularioNuevo.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const FormularioNuevo = () => {
-    return (  
-        <h1>Soy el formulario de nuevo producto</h1>
-    );
-}
- 
+  return (
+    <div>
+      <h1>AGREGAR PRODUCTO</h1>
+      <form>
+          <label>Nombre: </label>
+          <input type="text" name="name" placeholder="Nombre del producto"/>
+          <label>Precio: </label>
+          <input type="number" name="price" placeholder="Precio del producto"/>
+          <button type="button">Añadir producto</button>
+      </form>
+    </div>
+  );
+};
+
 export default FormularioNuevo;
