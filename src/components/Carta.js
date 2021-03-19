b9994454db0f30e37f99371c775accfaf31396bd
sarastackhouse <sarapalacios7@gmail.com>
2021-03-19 12:30:02 +0100

    changed useEffect in Carta

--- src/components/Carta.js
+++ src/components/Carta.js
@@ -13,7 +13,7 @@ const Carta = () => {
   useEffect(() => {
     const loadingProductos = () => dispatch(descargarProductosAction());
     loadingProductos();
-  }, [dispatch]);
+  }, [productos]);
 
   return (
     <div className="carta">
